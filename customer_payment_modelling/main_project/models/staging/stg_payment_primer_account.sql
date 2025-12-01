32ffb47bc7d656659b34329a3f8b7eaeb31d2fb1
Ayodeji <aayo.agunbiade@gmail.com>
2025-12-01 14:37:00 +0100

    Payment Primer Staging

--- customer_payment_modelling/main_project/models/staging/stg_payment_primer_account.sql
+++ customer_payment_modelling/main_project/models/staging/stg_payment_primer_account.sql
@@ -1,1 +1,15 @@
 
+{{ config(materialized='table') }}
+
+WITH source AS (
+    SELECT * 
+    FROM {{ source('raw_data', 'primer_account') }}
+
+)
+
+SELECT
+    primer_account_id::VARCHAR AS primer_account_id,
+    company_name::VARCHAR AS company_name,
+    created_at::TIMESTAMP AS created_at,
+    NOW()::TIMESTAMP AS ingestion_timestamp
+FROM source
